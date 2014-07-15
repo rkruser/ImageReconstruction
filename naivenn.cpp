dcc2f480b88739e082972a90ea2bd81f222aba23
Ryen Krusinga <rkruser@umich.edu>
2014-07-15 12:44:51 -0400

    Bug in previous commit fixed

--- naivenn.cpp
+++ naivenn.cpp
@@ -2,6 +2,7 @@
 #include "defs.h"
 
 #include <cmath>
+#include <iostream> //For debugging
 
 //Iterates over every pixel in the image
 //If the pixel is a nan, then set it equal to the average of the nearest kPixels
@@ -11,10 +12,10 @@ void naivenn(Image& M, int kPixels) {
 			for (size_t j = 0; j < M.cols; ++j) {
 				if (std::isnan(M(i,j,k))) {
 					spiralIterator iter(i,j);
-					iter.inc(); //Otherwise starts right on the pixel initialized with
 					int count = 0;
 					double accumulator = 0;
 					while (count < kPixels) {
+						iter.inc();
 						if (iter.x < 0 or iter.x >= M.rows
 								or iter.y < 0 or iter.y >= M.cols) {
 							continue;
@@ -23,7 +24,6 @@ void naivenn(Image& M, int kPixels) {
 							accumulator += M(iter.x,iter.y,k);
 							count++;
 						}
-						iter.inc();
 					}
 					M(i,j,k) = std::round(accumulator / kPixels);
 				}
